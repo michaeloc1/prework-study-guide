33b0409337590c1576076f824c48851f7dcf30c4
michaeloc1 <michaeloc@verizon.net>
2022-12-03 14:43:36 -0500

    added functions to scripts.js

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,29 @@
 
 
 var topics =['HTML', 'CSS', 'Git', 'Javascript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for(let i = 0; i < topics.length; i++){
+function listTopics(){
+    for(let i = 0; i < topics.length; i++){
 
-    console.log(topics[i]);
+        console.log(topics[i]);
+    }
 }
-/*
-if(topic === 'HTML'){
+
+function selectTopic(){
+if(randomTopic === 'HTML'){
     console.log("Let's study HTML");
 
 }
-else if(topic === 'CSS'){
+else if(randomTopic === 'CSS'){
     console.log("Let's study CSS");
 
 }
-else if(topic === 'Git'){
+else if(randomTopic === 'Git'){
     console.log("Let's study Git!");
 
 }
-else if(topic === 'Javascript'){
+else if(randomTopic === 'Javascript'){
     console.log("Let's study Javascript!");
 
 }
@@ -27,4 +31,8 @@ else{
     console.log("Please try again!");
 
 }
-*/
+}
+console.log('Here are the topics we learned in Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
